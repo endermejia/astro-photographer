8de93353b1c6053055cf4ed05f6841fbdd29d7e6
Nhoa <ainhoasanchezyepes16@gmail.com>
2026-07-01 12:49:41 +0200

    feat: title changes and style improves

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -25,7 +25,7 @@ export const routes = {
 
 export const ui = {
 	es: {
-		'meta.keywords': `nhoa noir, ainhoa sanchez, ainhoa sanchez yepes, nhoa noir fotografía, fotógrafa, fotógrafa profesional, fotógrafo en Castalla, Castalla, fotógrafo en Tibi, Tibi, fotógrafo en Ibi, Ibi, fotógrafo en Onil, Onil, fotógrafo en Alcoy, Alcoy, fotógrafo de bodas, fotógrafo de bodas en Castalla, fotógrafo profesional de bodas, fotografía de bodas, paquetes fotográficos de boda, fotografía de bodas de lujo, fotografía de bodas de alta gama, fotógrafo de personas, fotógrafo de personas en Castalla, fotografía de personas, fotógrafo de familia, fotógrafo de familia en Castalla, fotografía de familia, fotógrafo de niños, fotografía de niños, fotografía de bebés, fotografía de recién nacidos, fotógrafo de eventos, fotografía de eventos, fotógrafo de empresas, fotografía de estudio, fotografía artística, fotografía de estilo de vida, retratos, retratos de modelos, agencia de modelos, publicaciones,`,
+		'meta.keywords': `ainhoa sanchez, ainhoa sanchez yepes, ainhoa sanchez fotografía, fotógrafa, fotógrafa profesional, fotógrafo en Castalla, Castalla, fotógrafo en Tibi, Tibi, fotógrafo en Ibi, Ibi, fotógrafo en Onil, Onil, fotógrafo en Alcoy, Alcoy, fotógrafo de bodas, fotógrafo de bodas en Castalla, fotógrafo profesional de bodas, fotografía de bodas, paquetes fotográficos de boda, fotografía de bodas de lujo, fotografía de bodas de alta gama, fotógrafo de personas, fotógrafo de personas en Castalla, fotografía de personas, fotógrafo de familia, fotógrafo de familia en Castalla, fotografía de familia, fotógrafo de niños, fotografía de niños, fotografía de bebés, fotografía de recién nacidos, fotógrafo de eventos, fotografía de eventos, fotógrafo de empresas, fotografía de estudio, fotografía artística, fotografía de estilo de vida, retratos, retratos de modelos, agencia de modelos, publicaciones,`,
 		'nav.menu': `Menú`,
 		'nav.home': `Inicio`,
 		'nav.about': `Sobre mí`,
@@ -34,12 +34,12 @@ export const ui = {
 		'header.title': `Hola! Soy Ainhoa,`,
 		'header.subtitle': 'Escalada - Eventos - Retratos - Viajes',
 		'about.title': `📸 Sobre mí`,
-		'about.p1': `Soy Nhoa. Combino mis dos grandes amores: la fotografía y la escalada, para contar historias auténticas, ya sea en la cima de una montaña, en un evento vibrante o en la calidez de un retrato.`,
+		'about.p1': `Soy Ainhoa. Combino mis dos grandes amores: la fotografía y la escalada, para contar historias auténticas, ya sea en la cima de una montaña, en un evento vibrante o en la calidez de un retrato.`,
 		'about.p2': `Busco capturar la emoción real en cada instante, transmitiendo la esencia de cada persona, marca o lugar.`,
 		'about.p3': ``,
 		'about.p4': `¿Buscas a alguien que capture lo que te hace única/o?`,
 		'about.p5': `Estoy aquí para ayudarte a crear imágenes que inspiren y cuenten tu historia.`,
-		'about.contact': `Contactar con Nhoa`,
+		'about.contact': `Contactar con Ainhoa`,
 		'photos.title': 'Fotos',
 		'photos.climbing': 'Escalada',
 		'photos.people': 'Personas',
@@ -48,7 +48,7 @@ export const ui = {
 		'photos.travel': 'Viajes'
 	},
 	en: {
-		'meta.keywords': `nhoa noir, nhoa noir photography, photography, photographer, professional photographer, photographer in Castalla, Castalla, photographer in Tibi, Tibi, photographer in Ibi, Ibi, photographer in Onil, Onil, photographer in Alcoy, Alcoy, wedding photographer, wedding photographer in Castalla, professional wedding photographer, wedding photography, wedding photography packages, luxury wedding photography, high end wedding photography, people photographer, people photography, family photographer, family photography, kids photographer, kids photography, baby photography, newborn photography, events photographer, business photographer, studio photography, artistic photography, lifestyle photography, portraits, publications, `,
+		'meta.keywords': `ainhoa sanchez, ainhoa sanchez photography, photography, photographer, professional photographer, photographer in Castalla, Castalla, photographer in Tibi, Tibi, photographer in Ibi, Ibi, photographer in Onil, Onil, photographer in Alcoy, Alcoy, wedding photographer, wedding photographer in Castalla, professional wedding photographer, wedding photography, wedding photography packages, luxury wedding photography, high end wedding photography, people photographer, people photography, family photographer, family photography, kids photographer, kids photography, baby photography, newborn photography, events photographer, business photographer, studio photography, artistic photography, lifestyle photography, portraits, publications, `,
 		'nav.menu': `Menu`,
 		'nav.home': `Home`,
 		'nav.about': `About me`,
@@ -57,12 +57,12 @@ export const ui = {
 		'header.title': `Hi! I'm Ainhoa,`,
 		'header.subtitle': 'Climbing - Events - Portraits - Travel',
 		'about.title': `📸 About me`,
-		'about.p1': `I'm Nhoa. I combine my two great loves: photography and climbing, to tell authentic stories, whether at the top of a mountain, at a vibrant event, or in the warmth of a portrait.`,
+		'about.p1': `I'm Ainhoa. I combine my two great loves: photography and climbing, to tell authentic stories, whether at the top of a mountain, at a vibrant event, or in the warmth of a portrait.`,
 		'about.p2': `I seek to capture real emotion in every moment, transmitting the essence of every person, brand, or place.`,
 		'about.p3': ``,
 		'about.p4': `Looking for someone to capture what makes you unique?`,
 		'about.p5': `I'm here to help you create images that inspire and tell your story.`,
-		'about.contact': `Contact Nhoa`,
+		'about.contact': `Contact Ainhoa`,
 		'photos.title': 'Photos',
 		'photos.climbing': 'Climbing',
 		'photos.people': 'People',
